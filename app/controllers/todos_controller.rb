7a745e168ceb4d5ad88321208a6150491c620c68
aliabbasjaffri <ali.abbasjaffri@live.com>
2015-02-10 21:53:56 +0500

    Adding Bootstrap
    For styling

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,11 @@ class TodosController < ApplicationController
 	def Index
 		@todo_items = Todo.all
   	end
+
+  	def delete
+  		t = Todo
+  		t.delete(Todo.first)
+
+  		#@todo_items = Todo.all
+  	end
 end
